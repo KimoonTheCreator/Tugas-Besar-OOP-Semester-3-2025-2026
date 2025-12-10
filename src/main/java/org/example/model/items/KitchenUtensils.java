29b376bde7bbd58943a761d453188e74e877be8c
KimoonTheCreator <pangaribuanbram@gmail.com>
2025-12-10 18:12:44 +0700

    feat(model): add station logic

--- src/main/java/org/example/model/items/KitchenUtensils.java
+++ src/main/java/org/example/model/items/KitchenUtensils.java
@@ -4,40 +4,27 @@ import org.example.model.interfaces.Preparable;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public abstract class KitchenUtensils extends Item {
+
 
     protected Set<Preparable> contents;
 
-    public KitchenUtensils(String name){
+    public KitchenUtensils(String name) {
         super(name);
         this.contents = new HashSet<>();
-    }
-    public boolean isPortable(){
-        return true;
     }
 
     public Set<Preparable> getContents() {
         return contents;
     }
-    public void addItem(Preparable item){
+
+
+    public void addItem(Preparable item) {
         this.contents.add(item);
     }
-    public void  removeItem(){
+
+
+    public void emptyContents() {
         this.contents.clear();
-    }
-    public int capacity(){
-        //return kapasitas maksimal suatu bahan
-        return 0;
-    }
-    public boolean canAccept(Preparable ingredient){
-        //cek apakah bahan cocok dengan alat ini?
-        return false;
-    }
-    public void addIngredient(Preparable ingredient){
-        //masukkan bahan ke alat ini
-    }
-    public void startCooking(){
-        //ubah status bahan di dalamnya dari COOKING menjadi COOKED
     }
 }
